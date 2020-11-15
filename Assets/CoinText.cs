c161a6af391b8ad44a40c56f1fe4b6dfcdee80f4
Nikolay Bezdetniy <32260582+Azonos712@users.noreply.github.com>
2020-11-15 18:54:33 +0300

    Correct subscription and unsubscription of the event

--- Assets/CoinText.cs
+++ Assets/CoinText.cs
@@ -3,12 +3,12 @@ using UnityEngine.UI;
 public class CoinText : MonoBehaviour
 {
     Text _text;
-    void Awake()
+    private void OnEnable()
     {
         _text = GetComponent<Text>();
         GameManager.Instance.OnCollectCoin += ShowNumberOfCoins;
     }
-    private void OnDestroy()
+    private void OnDisable()
     {
         GameManager.Instance.OnCollectCoin -= ShowNumberOfCoins;
     }
